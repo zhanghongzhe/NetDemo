4244947af7f78eb187a90d6d034b0e5728563ebd
洪哲 张 <307032293@qq.com>
2020-04-26 18:01:24 +0800

    添加Excel读取和写入

---
+++ NetDemo.NPOI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NetDemo.NPOI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dt = ExcelHelper.ExcelToDataTable(@"D:\文档\20200423销售指标线上化\业绩指标模板-终版.xlsx", false);
+            ExcelHelper.DataTableToExcel(dt, @"D:\文档\20200423销售指标线上化\业绩指标模板-终版1.xlsx");
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
